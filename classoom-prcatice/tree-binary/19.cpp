3913f3dd958ca68ebf5ccff41c02bc9513df0bab
nishtha-thakkar <nishthathakkar3@gmail.com>
2026-04-01 10:58:41 +0530

    classroom-practice

---
+++ classoom-prcatice/tree-binary/19.cpp
@@ -0,0 +1,99 @@
+# include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class binaryTree{
+    public:
+    Node* root;
+
+    binaryTree(){
+        root = NULL;
+
+    }
+    void preOrder(Node* node){
+    if(node == NULL) return;
+
+    cout << node -> data << " ";
+    preOrder(node ->left);
+    preOrder(node ->right);
+
+
+
+
+};
+
+void inOrder(Node* node){
+     if(node == NULL) return;
+
+     inOrder(node ->left);
+     cout << node -> data << " ";
+     inOrder(node -> right);
+
+
+
+
+
+}
+
+void postOrder(Node* node){
+     if(node == NULL) return;
+    postOrder(node -> left );
+    postOrder(node -> right );
+    cout << node -> data << " ";
+
+
+
+}
+
+
+};
+
+
+
+
+int main(){
+    binaryTree b;
+
+    b.root = new Node(1);
+    b.root -> left = new Node (2);
+    b.root -> right = new Node (3);
+    b.root -> left ->left = new Node (-1);
+    b.root -> left ->right = new Node (-2);
+
+
+
+    cout << "preorder : ";
+    b.preOrder(b.root);
+
+
+    cout << "  \nInorder  : ";
+    b.inOrder(b.root);
+
+    cout << "  \npostorder: ";
+    b.postOrder(b.root);
+
+
+
+
+
+
+
+
+
+
+   
+
+
+}
